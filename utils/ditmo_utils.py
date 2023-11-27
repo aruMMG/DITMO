f01a898379663f5740d087575272999707f9cdb6
aru uni comp <aru1993s@gmail.com>
2023-11-27 16:19:19 +0000

    resize and padding function

--- utils/ditmo_utils.py
+++ utils/ditmo_utils.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import cv2
+from PIL import Image, ImageOps
 
 
 def imshow(image, title="image"):
@@ -47,3 +48,39 @@ def get_lower_bracket(im, factor):
     im = (im ** 2.2) / 2**factor
     im = im ** (1/2.2)
     return im
+
+def resize_and_pad_image(img, target_size=512):
+    """
+    Take a input image and return resized image to target size by keeping aspect ratio same. To achive the target size it pad on the smaller side.
+    Input:
+        img: the input image.
+        target_size
+
+    output:
+        return resized and padded image in PIL format.
+    """
+    if not isinstance(img, Image.Image):
+        try:
+            if isinstance(img, str):
+                img = Image.open(img)
+            else:
+                img = Image.fromarray(img)
+        except Exception as e:
+            print(f"Error in file format. The image instance is {type(img).__name__}. Exception: {str(e)}")
+    aspect_ratio = img.width / img.height
+
+    if img.width > img.height:
+        new_width = target_size
+        new_height = int(target_size / aspect_ratio)
+    else:
+        new_height = target_size
+        new_width = int(target_size * aspect_ratio)
+
+    resized_img = img.resize((new_width, new_height))
+
+    pad_width = target_size - resized_img.width
+    pad_height = target_size - resized_img.height
+
+    padded_img = ImageOps.expand(resized_img, border=(0, 0, pad_width, pad_height), fill=0)
+
+    return padded_img
